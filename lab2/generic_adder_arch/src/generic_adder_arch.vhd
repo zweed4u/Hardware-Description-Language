e38adb0cb03606d0dd94f04b73fec2fa5d3bc985
Zack Weeden <zweeden@datto.com>
2016-09-21 08:52:23 -0400

    fixing hierarchical adder

--- lab2/generic_adder_arch/src/generic_adder_arch.vhd
+++ lab2/generic_adder_arch/src/generic_adder_arch.vhd
@@ -1,31 +1,99 @@
 -------------------------------------------------------------------------------
+
 -- Dr. Kaputa
--- generic adder [behavioral]
+
+-- generic adder [hierarchical]
+
 -------------------------------------------------------------------------------
+
 library ieee;
+
 use ieee.std_logic_1164.all;
+
 use ieee.numeric_std.all;
 
-entity generic_adder_beh is
+
+
+entity generic_adder_arch is
+
   generic (
+
     bits    : integer := 4
+
   );
+
   port (
+
     a       : in  std_logic_vector(bits-1 downto 0);
+
     b       : in  std_logic_vector(bits-1 downto 0);
+
     cin     : in  std_logic;
+
     sum     : out std_logic_vector(bits-1 downto 0);
+
     cout    : out std_logic
+
   );
-end entity generic_adder_beh;
 
-architecture beh of generic_adder_beh is
+end entity generic_adder_arch;
 
-signal sum_temp   : std_logic_vector(bits downto 0);
-signal cin_guard  : std_logic_vector(bits-1 downto 0) := (others => '0');
+
+
+architecture arch of generic_adder_arch is
+
+
+
+component full_adder_single_bit_arch is 
+
+  port (
+
+    a       : in std_logic;
+
+    b       : in std_logic;
+
+    cin     : in std_logic;
+
+    sum     : out std_logic;
+
+    cout    : out std_logic
+
+  );
+
+end component full_adder_single_bit_arch;
+
+
+
+signal carry_internal: std_logic_vector(bits downto 0);
+
+
 
 begin
-  sum_temp  <= std_logic_vector(unsigned('0' & a) + unsigned('0' & b) + unsigned(cin_guard & cin));
-  sum       <= sum_temp(bits-1 downto 0);
-  cout      <= sum_temp(bits); -- Carry is the most significant bit
-end beh;
+
+  adders: for i in 0 to bits-1 generate
+
+    full_adder: full_adder_single_bit_arch
+
+      port map (
+
+        a       => a(i),
+
+        b       => b(i),
+
+        cin     => carry_internal(i),
+
+        sum     => sum(i),
+
+        cout    => carry_internal(i+1)
+
+      );
+
+  end generate;
+
+  
+
+  carry_internal(0) <= cin; 
+
+  cout <= carry_internal(bits);
+
+end arch;
