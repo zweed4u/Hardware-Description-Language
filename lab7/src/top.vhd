57410f6bee46f53233c4d8202a747dca9872cf8f
Zachary Weeden <zdw7287@rit.edu>
2016-11-30 23:15:27 -0500

    whitespace fix

--- lab7/src/top.vhd
+++ lab7/src/top.vhd
@@ -11,67 +11,69 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity top is
-  port ( 
-    clk             : in std_logic;
-    reset_n         : in std_logic;
-    execute         : in std_logic;
-    seven_seg_hun   : out std_logic_vector(6 downto 0);
-    seven_seg_ten   : out std_logic_vector(6 downto 0);
-    seven_seg_one   : out std_logic_vector(6 downto 0)
-  ); 
+    port ( 
+        clk             : in std_logic;
+        reset_n         : in std_logic;
+        execute         : in std_logic;
+        seven_seg_hun   : out std_logic_vector(6 downto 0);
+        seven_seg_ten   : out std_logic_vector(6 downto 0);
+        seven_seg_one   : out std_logic_vector(6 downto 0)
+    ); 
 end top;
 
 architecture beh of top is
 --ROM component here
 --Omit unused components - adjust this
 component blink_rom
-  PORT(
-    address         : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
-    clock           : IN STD_LOGIC  := '1';
-    q               : OUT STD_LOGIC_VECTOR (11 DOWNTO 0)
-  );
+    PORT(
+        address         : IN STD_LOGIC_VECTOR (3 DOWNTO 0);
+        clock           : IN STD_LOGIC  := '1';
+        q               : OUT STD_LOGIC_VECTOR (11 DOWNTO 0)
+    );
 end component;
 
 component seven_seg is
-  generic (
+    generic (
         max_count       : integer := 25000000
-  );
-  port (
-    bcd               : in std_logic_vector(3 downto 0);
-    seven_seg_out     : out std_logic_vector(6 downto 0)
-  );
+    );
+    port (
+        bcd               : in std_logic_vector(3 downto 0);
+        seven_seg_out     : out std_logic_vector(6 downto 0)
+    );
 end component; 
 
 component memory is
-  generic (addr_width : integer := 4; 
-           data_width : integer := 8);
-  port (
-    clk               : in std_logic;
-    we                : in std_logic;
-    addr              : in std_logic_vector(addr_width - 1 downto 0);
-    din               : in std_logic_vector(data_width - 1 downto 0);
-    dout              : out std_logic_vector(data_width - 1 downto 0)
-  );
+    generic (
+        addr_width : integer := 4; 
+        data_width : integer := 8
+    );
+    port (
+        clk               : in std_logic;
+        we                : in std_logic;
+        addr              : in std_logic_vector(addr_width - 1 downto 0);
+        din               : in std_logic_vector(data_width - 1 downto 0);
+        dout              : out std_logic_vector(data_width - 1 downto 0)
+    );
 end component; 
 
 component rising_edge_synchronizer is 
-  port (
-    clk               : in std_logic;
-    reset             : in std_logic;
-    input             : in std_logic;
-    edge              : out std_logic
-  );
+    port (
+        clk               : in std_logic;
+        reset             : in std_logic;
+        input             : in std_logic;
+        edge              : out std_logic
+    );
 end component;
 
 component alu is
-  port (
-    clk        :in std_logic;
-    reset      :in std_logic;
-    a          :in std_logic_vector(7 downto 0);
-    b          :in std_logic_vector(7 downto 0);
-    op         :in std_logic_vector(1 downto 0);
-    result     :out std_logic_vector(7 downto 0)
-  );
+    port (
+        clk        :in std_logic;
+        reset      :in std_logic;
+        a          :in std_logic_vector(7 downto 0);
+        b          :in std_logic_vector(7 downto 0);
+        op         :in std_logic_vector(1 downto 0);
+        result     :out std_logic_vector(7 downto 0)
+    );
 end component;
 
 --INTERNAL SIGNALS
@@ -121,73 +123,71 @@ begin
 --ROM instantiation here
 --Omit unneeded instances - MAP INTERNAL SIGNALS TO EITHER PROCESS OR OTHER COMPONENTS 
 rom_inst : blink_rom 
-  port map (
-    address     => address_sig,
-    clock       => clk,
-    q           => concatenated_op_input
-  );
+    port map (
+        address     => address_sig,
+        clock       => clk,
+        q           => concatenated_op_input
+    );
 
 alu_comp: alu 
-  port map(
-    clk         => clk,
-    reset       => reset_n,
-    a           => save, 
-    b           => input, --alias from rom not synched
-    op          => opCode,
-    result      => result_sig
-  );
+    port map(
+        clk         => clk,
+        reset       => reset_n,
+        a           => save, 
+        b           => input, --alias from rom not synched
+        op          => opCode,
+        result      => result_sig
+    );
  
 sync_execute: rising_edge_synchronizer 
-  port map(
-    clk     => clk,
-    reset   => reset_n,
-    input   => execute,
-    edge    => synced_execute
-  );
+    port map(
+        clk     => clk,
+        reset   => reset_n,
+        input   => execute,
+        edge    => synced_execute
+    );
 
 convert_to_ssd_a: seven_seg 
     port map(
-      bcd           => hundreds, --hundreds from dd
-      seven_seg_out => seven_seg_hun
+        bcd           => hundreds, --hundreds from dd
+        seven_seg_out => seven_seg_hun
     );
 
 convert_to_ssd_b: seven_seg 
     port map(
-      bcd           => tens, --tens from dd
-      seven_seg_out => seven_seg_ten
-    );
+        bcd           => tens, --tens from dd
+        seven_seg_out => seven_seg_ten
+    ;
 
 convert_to_ssd_c: seven_seg 
     port map(
-      bcd           => ones, --ones from dd
-      seven_seg_out => seven_seg_one
+        bcd           => ones, --ones from dd
+        seven_seg_out => seven_seg_one
     );
 
 comp_memory: memory 
     port map(
-      clk  => clk,
-      we   => output_logic_we,
-      addr => output_logic_addr,
-      din  => to_mem,
-      dout => save
+        clk  => clk,
+        we   => output_logic_we,
+        addr => output_logic_addr,
+        din  => to_mem,
+        dout => save
     );
 
 --process to fetch on execute and parse 12 bit qsig from rom component with aliases - ADJUST THIS
 update_address: process(clk,reset_n,address_sig)
-  begin
+begin
     if reset_n = '0' then
-      address_sig <= (others => '0');
+        address_sig <= (others => '0');
     elsif clk'event and clk = '1' then
-      if synced_execute = '1' then
-        address_sig <= std_logic_vector(unsigned(address_sig) + 1 );
-      end if;
+        if synced_execute = '1' then
+            address_sig <= std_logic_vector(unsigned(address_sig) + 1 );
+        end if;
     end if;
 end process;
 
-
 --PARSE ROM SIGNAL - maybe synch signals?
 --parse_rom_signal: process(synced_execute, clk, reset_n)
-
 
 --state register
 process(reset_n, clk) --SR 
@@ -216,9 +216,9 @@ begin
                 elsif (ms='1') then --not synched
                     stateNext <= write_r;
                 elsif (mr='1') then --not synched
-					--preparing memory for mr (read_r state)
-					output_logic_we <= '0';
-					output_logic_addr <= "0001";
+                    --preparing memory for mr (read_r state)
+                    output_logic_we <= '0';
+                    output_logic_addr <= "0001";
                     stateNext <= read_r;
                 else 
                     stateNext <= read_w;
@@ -242,14 +242,14 @@ begin
                 --led <= "01000";
                 stateNext <= write_w;
 
-			when write_mem_to_work => 
-				output_logic_we <= '1';
+            when write_mem_to_work => 
+                output_logic_we <= '1';
                 output_logic_addr <= "0000";
-				stateNext <= write_w; --continue on the read_w -> read_r -> read_w chain
+                stateNext <= write_w; --continue on the read_w -> read_r -> read_w chain
 
             when write_w =>
                 --led <= "10000";
-				output_logic_we <= '1';
+                output_logic_we <= '1';
                 output_logic_addr <= "0000";
                 stateNext <= read_w;
 
@@ -277,21 +277,21 @@ end process;
 
 --DOUBLE DABBLE PROCESS - takes 8 bit number and parses into hundreds, tens,and ones
 bcd1: process(preDD)
-  -- temporary variable
-  variable temp : STD_LOGIC_VECTOR (11 downto 0);
-  
-  -- variable to store the output BCD number
-  -- organized as follows
-  -- thousands = bcd(15 downto 12)
-  -- hundreds = bcd(11 downto 8)
-  -- tens = bcd(7 downto 4)
-  -- units = bcd(3 downto 0)
-  variable bcd : UNSIGNED (15 downto 0) := (others => '0');
+-- temporary variable
+variable temp : STD_LOGIC_VECTOR (11 downto 0);
 
-  -- by
-  -- https://en.wikipedia.org/wiki/Double_dabble
-  
-  begin
+-- variable to store the output BCD number
+-- organized as follows
+-- thousands = bcd(15 downto 12)
+-- hundreds = bcd(11 downto 8)
+-- tens = bcd(7 downto 4)
+-- units = bcd(3 downto 0)
+variable bcd : UNSIGNED (15 downto 0) := (others => '0');
+
+-- by
+-- https://en.wikipedia.org/wiki/Double_dabble
+
+begin
     -- zero the bcd variable
     bcd := (others => '0');
     
@@ -302,27 +302,27 @@ bcd1: process(preDD)
     -- this could be optimized, we dont need to check and add 3 for the 
     -- first 3 iterations as the number can never be >4
     for i in 0 to 11 loop
-      if bcd(3 downto 0) > 4 then 
-        bcd(3 downto 0) := bcd(3 downto 0) + 3;
-      end if;
+        if bcd(3 downto 0) > 4 then 
+            bcd(3 downto 0) := bcd(3 downto 0) + 3;
+        end if;
       
-      if bcd(7 downto 4) > 4 then 
-        bcd(7 downto 4) := bcd(7 downto 4) + 3;
-      end if;
+        if bcd(7 downto 4) > 4 then 
+            bcd(7 downto 4) := bcd(7 downto 4) + 3;
+        end if;
     
-      if bcd(11 downto 8) > 4 then  
-        bcd(11 downto 8) := bcd(11 downto 8) + 3;
-      end if;
+        if bcd(11 downto 8) > 4 then  
+           bcd(11 downto 8) := bcd(11 downto 8) + 3;
+        end if;
 
-      -- thousands can't be >4 for a 12-bit input number
-      -- so don't need to do anything to upper 4 bits of bcd
-    
-      -- shift bcd left by 1 bit, copy MSB of temp into LSB of bcd
-      bcd := bcd(14 downto 0) & temp(11);
-    
-      -- shift temp left by 1 bit
-      temp := temp(10 downto 0) & '0';
-    
+        -- thousands can't be >4 for a 12-bit input number
+        -- so don't need to do anything to upper 4 bits of bcd
+      
+        -- shift bcd left by 1 bit, copy MSB of temp into LSB of bcd
+        bcd := bcd(14 downto 0) & temp(11);
+      
+        -- shift temp left by 1 bit
+        temp := temp(10 downto 0) & '0';
+
     end loop;
  
     -- set outputs
@@ -330,6 +330,6 @@ bcd1: process(preDD)
     tens <= STD_LOGIC_VECTOR(bcd(7 downto 4));
     hundreds <= STD_LOGIC_VECTOR(bcd(11 downto 8));
     --thousands <= STD_LOGIC_VECTOR(bcd(15 downto 12));
-  end process bcd1;
+end process bcd1;
 
 end beh;
