d7cd7e59b1a8d66f90948c1b9eb2c02d54281222
Zachary Weeden <zackweeden@aol.com>
2016-12-08 12:08:09 -0500

    Name changes in top to match wave.do & small fixes

--- lab8/src/audio_processor_3000.vhd
+++ lab8/src/audio_processor_3000.vhd
@@ -45,13 +45,19 @@ component rising_edge_synchronizer is
     );
 end component;
 
-signal synced_execute       : std_logic;
+signal edge       : std_logic;
 
-signal data_address         : std_logic_vector(13 downto 0);
+signal data_address             : std_logic_vector(13 downto 0);
+signal data_address_reg         : std_logic_vector(13 downto 0); --after state reg
+signal data_address_play        : std_logic_vector(13 downto 0);
+signal data_address_play_repeat : std_logic_vector(13 downto 0);
+signal data_address_stop        : std_logic_vector(13 downto 0);
+signal data_address_pause       : std_logic_vector(13 downto 0);
+signal data_address_seek        : std_logic_vector(13 downto 0);
+
 signal data_out             : std_logic_vector(15 downto 0); --this is audio_out internal signal not needed
-
-signal other_da             : std_logic_vector(4 downto 0); --for other memory instantiation?
-signal rom_instruct_out     : std_logic_vector(7 downto 0); --for other memory instantiation?
+signal pc                   : std_logic_vector(4 downto 0); --for other memory instantiation data address
+signal rom_instruct_out     : std_logic_vector(7 downto 0); --for other memory instantiation instruction out 
 
 --States
 constant idle           : std_logic_vector(4 downto 0) :="00001";
@@ -59,8 +65,9 @@ constant fetch          : std_logic_vector(4 downto 0) :="00010";
 constant decode         : std_logic_vector(4 downto 0) :="00100";
 constant execute        : std_logic_vector(4 downto 0) :="01000";
 constant decode_error   : std_logic_vector(4 downto 0) :="10000";
-signal stateReg         : std_logic_vector(4 downto 0);
-signal stateNext        : std_logic_vector(4 downto 0);
+signal instruction      : std_logic_vector(7 downto 0);
+signal state_reg        : std_logic_vector(4 downto 0);
+signal state_next       : std_logic_vector(4 downto 0);
 
 --Instruction set
 constant play           : std_logic_vector(1 downto 0) :="00";
@@ -69,9 +76,9 @@ constant seek           : std_logic_vector(1 downto 0) :="10";
 constant stop           : std_logic_vector(1 downto 0) :="11";
 
 --Aliases for instruction signal
-alias op 	: std_logic_vector(2 downto 0) is rom_instruct_out(7 downto 6);
-alias rpt 	: std_logic is rom_instruct_out(5);
-alias sk	: std_logic_vector(4 downto 0) is rom_instruct_out(4 downto 0)
+alias opcode        : std_logic_vector(1 downto 0) is rom_instruct_out(7 downto 6);
+alias rpt           : std_logic is rom_instruct_out(5);
+alias seek_address  : std_logic_vector(4 downto 0) is rom_instruct_out(4 downto 0);
 
 begin
 -- data instantiation
@@ -85,7 +92,7 @@ u_rom_data_inst : rom_data
 --other rom instantiaion needed?
 u_rom_instr_inst : rom_instructions
     port map (
-        address    => other_da,--5bits
+        address    => pc,--5bits
         clock      => clk,
         q          => rom_instruct_out --8bits
     );
@@ -96,131 +103,130 @@ sync_execute: rising_edge_synchronizer
         clk     => clk,
         reset   => reset,
         input   => execute_btn,
-        edge    => synced_execute
+        edge    => edge
     );
 
 --state register
 process(reset, clk)
 begin
     if (reset = '1') then
-        stateReg <= idle;
+        state_reg <= idle;
     elsif (clk'event and clk ='1') then
-        stateReg <= stateNext;
+        state_reg <= state_next;
     end if;
 end process;
 
 --sensitivity list Next State Logic
-process(stateReg, reset, synced_execute, op, sk) --NSL
+process(state_reg, reset, edge, opcode, seek_address) --NSL
 begin
     if reset = '1' then 
-        stateNext <= idle;
+        state_next <= idle;
     else 
-        stateNext <= stateReg; --prevent latch
-        case stateReg is
+        state_next <= state_reg; --prevent latch
+        case state_reg is
             when idle =>
-                if (synced_execute='1') then
-                    stateNext <= fetch;
+                if (edge='1') then
+                    state_next <= fetch;
                 else 
-                    stateNext <= idle;
+                    state_next <= idle;
                 end if;
 
             when fetch =>
-				--get the instruction from mem
-                stateNext <= decode;
+                --get the instruction from mem
+                state_next <= decode;
 
             when decode =>
-				if op="10" then --seeking
-					if sk="0000" then --seeking instruction going to 00000000000000
-						--not allowed to seek to the beginning go to decode_error
-						stateNext <= decode_error;
-					end if;
+                if opcode="10" then --seeking
+                    if seek_address="0000" then --seeking instruction going to 00000000000000
+                        --not allowed to seek to the beginning go to decode_error
+                        state_next <= decode_error;
+                    end if;
                 else
-					stateNext <= execute;
-				end if;
+                    state_next <= execute;
+                end if;
 
             when decode_error =>
-                stateNext <= idle;	
+                state_next <= idle;    
 
             when execute =>
-                if (synced_execute='1') then
-                    stateNext <= fetch;
+                if (edge='1') then
+                    state_next <= fetch;
                 end if;
 
             when others =>
-                stateNext <= idle;
+                state_next <= idle;
         end case;
     end if;
 end process;
 
---
---FU's here? (async) --14 bit signals?
---data_address_play<=data_address+1
---data_address_play_repeat<=data_address+1
---stop<=(others => '0')
---pause<=data_address
---seek<=data_address --sk+"000000000"
---
---process(a,b,reset)
---begin
---  if reset = '1' then 
---    idle_sig  <= "00";  
---    add_sig   <= "00";
---    sub_sig   <= "00";
---  else 
---    idle_sig  <= "00";  
---    add_sig   <= std_logic_vector(unsigned(a) + unsigned(b));
---    sub_sig   <= std_logic_vector(unsigned(a) - unsigned(b));
---  end if;
---end process;
---
+--FU's here (async) --14 bit signals?
+process(data_address_reg,seek_address,reset)--data_address? (sensitivity list)
+begin
+    data_address_play_repeat  <= std_logic_vector(unsigned(data_address_reg)+1); 
+    data_address_play<= std_logic_vector(unsigned(data_address_reg)+1);
+    data_address_pause<=data_address_reg;
+    data_address_stop<=(others => '0');
+    data_address_seek<=seek_address&"000000000";
+end process;
+
+-- data register [synchronous]
+process(clk,reset)
+begin 
+  if (reset = '1') then 
+    data_address_reg <= (others => '0');
+  elsif (clk'event and clk = '1') then
+    data_address_reg <= data_address;
+  end if;
+end process;
 
 --Need another PC for u_rom_inst : rom_instructions?
-update_address: process(clk, reset, synced_execute, other_da) --name of this signal? (other_da) sensitivity list?
+update_address: process(clk, reset, edge, pc) --name of this signal? (pc) sensitivity list?
 begin
     if reset = '1' then
-        other_da <= (others => '0');
+        pc <= (others => '0');
     elsif clk'event and clk = '1' then
-        if synced_execute = '1' then
-            other_da <= std_logic_vector(unsigned(other_da) + 1 );
+        if edge = '1' then
+            pc <= std_logic_vector(unsigned(pc) + 1 );
         end if;
     end if;
 end process;
 
 --Mux process - defined as follows
-process(op, rpt, data_address_play, data_address_play_repeat, stop, pause, seek, data_address) --sk alias/other signals in sensitivity list? 
+process(opcode, rpt, data_address_play, data_address_play_repeat, data_address_stop, data_address_pause, data_address_seek, data_address) --seek_address alias/other signals in sensitivity list? 
 begin
-    case op is
+    case opcode is
         when play => --00
             if rpt='1' then
                 data_address<=data_address_play_repeat; --is this assignment correct and where is the the signal being 'given'? (14 bits)
             else
-                data_address<=data_address_play;        --is this assignment correct and where is the the signal being 'given'?
+                data_address<=data_address_play;
             end if;
 
         when pause => --01
-            data_address<=pause;                        --is this assignment correct and where is the the signal being 'given'?
+            data_address<=data_address_pause;
 
         when seek => --10
-            data_address<=seek;                         --is this assignment correct and where is the the signal being 'given'?
+            data_address<=data_address_seek;
 
         when stop => --11
-            data_address<=stop;                         --is this assignment correct and where is the the signal being 'given'?
+            data_address<=data_address_stop;
 
         when others =>
-            data_address<=data_address;                 --is this assignment correct and where is the the signal being 'given'?
+            data_address<=data_address;
     end case;
+end process;
 
 -- loop audio file (data register as mentioned in email?)
 process(clk,reset) --sync needed in sensitivity list?
 begin 
-	if (reset = '1') then 
-		data_address <= (others => '0');
-	elsif (clk'event and clk = '1') then
-		if (sync = '1') then    
-			data_address <= std_logic_vector(unsigned(data_address) + 1 ); -- data_address vs. da_reg
-		end if;
-	end if;
+    if (reset = '1') then 
+        data_address <= (others => '0');
+    elsif (clk'event and clk = '1') then
+        if (sync = '1') then    
+            data_address <= std_logic_vector(unsigned(data_address) + 1 ); -- data_address vs. data_address_reg
+        end if;
+    end if;
 end process;
-
+instruction<=rom_instruct_out;
 led <= "10101010";
 end beh;
